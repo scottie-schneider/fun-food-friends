28fdbb99faf7419861d3e019925d33806d637113
Scottie Schneider <scott@laclave.co>
2017-09-24 18:26:58 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentItem: '',
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>Fun Food Friends</h1>
+
+            </div>
+        </header>
+        <div className='container'>
+          <section className='add-item'>
+              <form>
+                <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+                <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem}/>
+                <button>Add Item</button>
+              </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyBnxcjjM49H8L5Ic1qDwD-D8sO_451fE10",
+    authDomain: "fun-food-friends-6fbfe.firebaseapp.com",
+    databaseURL: "https://fun-food-friends-6fbfe.firebaseio.com",
+    projectId: "fun-food-friends-6fbfe",
+    storageBucket: "",
+    messagingSenderId: "289270258687"
+  };
+firebase.initializeApp(config);
+export default firebase;
