eb1b258318de951d3d468978158139c983b26de8
Scottie Schneider <scott@laclave.co>
2017-09-24 18:50:05 -0500

    Persisting data to firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       currentItem: '',
-      username: ''
+      username: '',
+      items: []
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e){
     this.setState({
       [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: ''
     });
   }
 
@@ -29,7 +45,7 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
                 <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem}/>
                 <button>Add Item</button>
